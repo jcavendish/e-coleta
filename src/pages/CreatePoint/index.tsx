24eb4702a594c779cd5e0bd3b3cf62f79c53b16f
Joao Paulo Cavendish <joaopaulocavendish@gmail.com>
2020-07-23 12:20:17 -0300

    integrar leaflet map

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -60,6 +61,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-8.4264296,-34.9837303]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-8.4264296,-34.9837303]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
